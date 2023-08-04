125af39400dae699ed5fb7d24291a14aa82462c0
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-08-04 16:53:28 +0200

    nodemon

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const fs = require('fs');
-const path = require('path');
+const path = require('path');  /*route*/
 const express = require('express');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/currenttime', function (req, res) {
     res.send('<h1>' + new Date().toISOString() + '</h1>');
 });
 
 app.get('/', function (req, res) {
-    res.send('<form action="/store-user" method="POST"><label>Your name</label><input type="text" name="username"><button>Submit</button></form>');
+    res.send('<form action="/store-user" method="POST"><label>Your name: </label><input type="text" name="username"><button>Submit</button></form>');
 });
 
-app.post('/store-user', function(req, res) {
-const userName = req.body.username;
+app.post('/store-user', function (req, res) {
+    const userName = req.body.username;
 
-const filePath = path.join(__dirname, 'data', 'users.json');
+    const filePath = path.join(__dirname, 'data', 'users.json'); /*fichier vers lequel les donnnées vont être envoyées en text raw*/
 
-const fileData = fs.readFileSync(filePath);
-const existingUsers = JSON.parse(fileData);
+    const fileData = fs.readFileSync(filePath); /*pointe/lit les données du filePath*/
+    const existingUsers = JSON.parse(fileData); /* transforme json de fileData en objet JS*/
 
-existingUsers.push(userName);
+    existingUsers.push(userName);
 
-fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+    fs.writeFileSync(filePath, JSON.stringify(existingUsers)); /*transforme JS en text*/
 
-res.send('<h1>Username stored!</h1>');
+    res.send('<h1>Username stored!</h1>');
 })
+
+app.get('/user', function (req, res) {
+    const filePath = path.join(__dirname, 'data', 'users.json'); /*fichier vers lequel les donnnées vont être envoyées en text raw*/
+
+    const fileData = fs.readFileSync(filePath); /*pointe/lit les données du filePath*/
+    const existingUsers = JSON.parse(fileData); /* transforme json de fileData en objet JS*/
+
+    let responseData = '<ul>';
+
+    for (const user of existingUsers) {
+        responseData += '<li>' + user + '</li>';
+    }
+    responseData += '</ul>';
+    
+    res.send(responseData);
+});
 
 app.listen(3000);
